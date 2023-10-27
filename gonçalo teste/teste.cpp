159162fc2fb64831451e60a8f9c27c6f08083883
GNC <gfmoniz008@gmail.com>
2023-10-27 10:31:04 +0000

    euro milhoes

--- gonçalo teste/teste.cpp
+++ gonçalo teste/teste.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <cmath>
+#include "asminhascontas.h"
 using namespace std;
 
 int a=2, b=3;
-
-int main() {
-    cout << max(a,b) << endl;
+int main(){
+    cout << oDobro(2) << endl;
+    xpto();
     return 0;
 }
 
@@ -22,51 +23,48 @@ bool O_MEU_BOOLEANO = true;
 
 //tipo de variavel nome da variavel - valor da variavel
 
-const double PI = 3.1415926553589793238463                             //variavel inteira
-int x = 2, y=3, z=14;                             //constante 
-int minutosPorHora= 60;
-int numero;
+//const double PI = 3.1415926553589793238463                             //variavel inteira
+//int x = 2, y=3, z=14;                             //constante 
+//int minutosPorHora= 60;
+//int numero;
 
-int main()
-{
+//int cenas(){
     //tendo em consideração que a área do circulo
     //é PI * raio, calcular a área do circulo
     //com raio 10
-    int a = "1";
-    int b = "2";
-    double área = ?
-    int r = 8;
-    double área = PI * r * r;
-    int raio;
-    cout << "Qual o raio do circulo" <<endl; 
-    cin >> raio;
-    double area = PI * raio * raio; 
-    cout << a+b << endl; //imprime no ecrã
-    cin >> numero;                //lê do teclado
-    cout<< "O numero que digitaste foi: " << numero << endl;
-    int a = 1000;
-    int b = 2000;
-    int c = b-a;
+    //int a = "1";
+    //int b = "2";
+    //double área = ?
+    //int r = 8;
+    //double área = PI * r * r;
+    //int raio;
+    //cout << "Qual o raio do circulo" <<endl; 
+    //cin >> raio;
+    //double area = PI * raio * raio; 
+    //cout << a+b << endl; //imprime no ecrã
+    //cin >> numero;                //lê do teclado
+    //cout<< "O numero que digitaste foi: " << numero << endl;
+    //int a = 1000;
+    //int b = 2000;
+    //int c = b-a;
 
-    int a = 7;
-    int b = 5;
-    int c = 2;
-    int d = 1;
-    int resultado = a*=2;                //a = a mais 6 a+6 // a = a * vezes 2
-    cout << resultado << endl;
+    //int a = 7;
+    //int b = 5;
+    //int c = 2;
+    //int d = 1;
+    //int resultado = a*=2;                //a = a mais 6 a+6 // a = a * vezes 2
+    //cout << resultado << endl;
    
 
 
-    return 0;
-}
+//}
 
 /*
 * @author sou eu
 * teste
 *
 */
-int main()
-{
+int coisas(){
     cout << "Hello \\ World!\n"; //isto um comentário
     cout << "I'm a C++ program\n";
     cout << O_MEU_NUMERO;
